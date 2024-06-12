cf6988c22d12482dcc96a969db1f691ac7928cad
Lucas-FA <lucasfreitasdeabreu@hotmail.com>
2024-06-12 15:39:38 -0300

    feat: colisão com as paredes implementada

--- src/scenes/gamificationScene.ts
+++ src/scenes/gamificationScene.ts
@@ -1,9 +1,27 @@
-import { Actor, Color, Engine, Scene, vec } from "excalibur";
+import { Actor, Color, Engine, Keys, Scene, SceneActivationContext, vec } from "excalibur";
 import { Resources } from "../resources";
 
 export class gamificationScene extends Scene {
     // Declaração do elemento texto sobre gamificação
     elementoTextoGamificacao?: HTMLElement
+
+    // Método para esmaecer um elemento HTML
+    fadeOutElement(elemento: HTMLElement) {
+        // Pegar opacidade do elemento HTML
+        let opacidade = parseFloat(elemento.style.opacity)
+
+        // Repetir diminuição da opacidade
+        setInterval(() => {
+            // Se elemento ainda está visível
+            if (opacidade > 0) {
+                // Diminuir a opacidade
+                opacidade -= 0.03
+    
+                // Atualizar a opacidade do elemento
+                elemento.style.opacity = opacidade.toString()
+            }
+        }, 20)
+    }
 
     onInitialize(engine: Engine<any>): void {
         this.backgroundColor = Color.Gray
@@ -16,7 +34,7 @@ export class gamificationScene extends Scene {
 
         // Inserir elementoTextoGamificacao no container-game
         let containerGame = document.querySelector(".container-game") as HTMLElement
-        containerGame.appendChild(this.elementoTextoGamificacao)
+        containerGame?.appendChild(this.elementoTextoGamificacao)
 
         // Adicionar classe na div criada (elementoTextoGamificacao)
         this.elementoTextoGamificacao.classList.add("sobre-gamificacao")
@@ -24,6 +42,8 @@ export class gamificationScene extends Scene {
         // Adicionar título e parágrafo dentro do conteúdo da div
         this.elementoTextoGamificacao.innerHTML = `<h2>O que é gamificação?</h2>
         <p>Gamificação é a aplicação de elementos típicos de jogos em contextos não lúdicos, com o objetivo de engajar e motivar indivíduos a atingir determinados objetivos. Esta abordagem se utiliza de componentes como pontuação, níveis, recompensas, desafios, e feedback imediato, visando promover comportamentos desejados e aumentar a participação e o comprometimento dos participantes.</p>`
+
+        this.elementoTextoGamificacao.classList.add("gamificacao")
 
         // Adicionar imagem de gamificação
         //Configurar actor da imagem de gamificação
@@ -40,5 +60,20 @@ export class gamificationScene extends Scene {
 
         // Renderizar o actor na cena
         this.add(actorImagemGamificacao)
+
+        // Configurar a cena para monitorar o evento de tecla pressionada
+        this.input.keyboard.on("press", (event) => {
+            if (event.key == Keys.Enter) {
+                // Criar transição suave do elemento texto / ? - elemento pode ser indefinido/nulo; ! - diz que o elemento não vai ser nulo
+                this.fadeOutElement(this.elementoTextoGamificacao!)
+
+                // Direcionar para a próxima cena
+                engine.goToScene("gamificacao")
+            }
+        })
+    }
+
+    onDeactivate(context: SceneActivationContext<undefined>): void {
+        this.elementoTextoGamificacao?.remove()
     }
 }
